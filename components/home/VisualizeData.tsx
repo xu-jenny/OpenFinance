b5ee42de3a9bfc0704dddba4331e3c812945099b
xu-jenny <jenny>
2023-06-27 22:57:37 -0400

    play with data viz

--- components/home/VisualizeData.tsx
+++ components/home/VisualizeData.tsx
@@ -17,16 +17,38 @@ const DynamicTable = ({ data }: { data: any }) => {
     }, [data]);
 
     return (
-        <div>
+        <>
             <Table data={data} columns={columns} paginate={true} />
-        </div>
+        </>
     )
 }
 
 export const VisualizeData = ({ data }: { data: any }) => {
-    return (<>
-        <DynamicTable data={data} />
-        <TimeSeriesChart data={data}/>
-    </>)
+    const formatDate = (time: string, isWeek: boolean = false): Date => {
+        if (isWeek) {
+          const [year, week] = time.split('-');
+          const date = new Date(`${year}-01-01`);
+          date.setDate(date.getDate() + (parseInt(week) - 1) * 7);
+          // const options = { week: '2-digit', year: 'numeric' };
+          return new Date(date);
+        }
+        return new Date(time);
+      };
+    
+      const transformedData = data.map((item: { [x: string]: any }) => ({
+        ...item,
+        // @ts-ignore
+        time:
+          'week' in item
+            ? formatDate(item['week'], true)
+            : formatDate(item['month'] || item['day']),
+      }));
+      transformedData.sort((a, b) => a.time.getTime() - b.time.getTime());
+    return (<div className="flex flex-col justify-center items-center gap-4">
+        <div className="p-3 flex flex-col items-center">
+            <DynamicTable data={transformedData} />
+        </div>
+        <TimeSeriesChart data={transformedData}/>
+    </div>)
 }
 
