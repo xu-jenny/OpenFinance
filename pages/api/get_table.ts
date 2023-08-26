fe921934f6f5a5ad390b4d0d44902eef04d315f9
xu-jenny <jenny>
2023-08-26 09:42:51 -0400

    fix build errors and migrate

--- pages/api/get_table.ts
+++ pages/api/get_table.ts
@@ -1,42 +1,47 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { prismaCli } from "@/prisma/prisma";
-import { TableNames } from "./discover";
-import { supabaseClient } from "@/utils/supabase-client";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prismaCli } from '@/prisma/prisma';
+import { supabaseClient } from '@/utils/supabase-client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { TableNames } from '@/prompts/types';
 
 async function getTable(tableName: string) {
-    // find download link in supabase
-    const { data } = supabaseClient
-        .storage
-        .from('tables')
-        .getPublicUrl(`${tableName}.csv`)
-        return data
-  }
+  // find download link in supabase
+  const { data } = supabaseClient.storage
+    .from('tables')
+    .getPublicUrl(`${tableName}.csv`);
+  return data;
+}
 
 function isValidTableName(name: string): name is TableNames {
-    return Object.values(TableNames).includes(name as TableNames);
-  }
+  return Object.values(TableNames).includes(name as TableNames);
+}
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
-  ) {
-    console.log("get table endpoint")
-    const tableName = req.query.tableName as string;
-    console.log(tableName)
-    if(!tableName){
-        return res.status(400).json({ message: 'No table name in the request param' });
-    }
-    try {
-        let row = await prismaCli.tableMetaData.findFirstOrThrow({ where: { tableName } })
-        res.status(200).json({ data: row.tableName })
-    } catch (e){
-        if (e instanceof PrismaClientKnownRequestError){
-            console.error("cannot find tablename", tableName,  e)
-            res.status(400).json({ message: 'Tablename provided does not exist' });
-        } else {
-            console.error("unknown error fetching tableMetaData", e)
-            res.status(400).json({ message: 'Server error, please try again later!' });
-        }
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  console.log('get table endpoint');
+  const tableName = req.query.tableName as string;
+  console.log(tableName);
+  if (!tableName) {
+    return res
+      .status(400)
+      .json({ message: 'No table name in the request param' });
+  }
+  try {
+    let row = await prismaCli.tableMetaData.findFirstOrThrow({
+      where: { tableName },
+    });
+    res.status(200).json({ data: row.tableName });
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      console.error('cannot find tablename', tableName, e);
+      res.status(400).json({ message: 'Tablename provided does not exist' });
+    } else {
+      console.error('unknown error fetching tableMetaData', e);
+      res
+        .status(400)
+        .json({ message: 'Server error, please try again later!' });
     }
   }
+}
--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -15,18 +15,6 @@ CREATE TABLE "WarnNotice" (
 );
 
 -- CreateTable
-CREATE TABLE "Question" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "question" TEXT NOT NULL,
-    "llm_response" TEXT,
-    "error" TEXT,
-    "uid" UUID NOT NULL,
-
-    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "User" (
     "id" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -39,11 +27,86 @@ CREATE TABLE "User" (
 CREATE TABLE "TableMetaData" (
     "id" SERIAL NOT NULL,
     "tableName" TEXT NOT NULL,
-    "promptSchema" TEXT NOT NULL,
+    "downloadInstruction" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
 
     CONSTRAINT "TableMetaData_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
-ALTER TABLE "Question" ADD CONSTRAINT "Question_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE "MinneapolisCrimeRate" (
+    "X" DOUBLE PRECISION NOT NULL,
+    "Y" DOUBLE PRECISION NOT NULL,
+    "Type" TEXT NOT NULL,
+    "Case_Number" TEXT NOT NULL,
+    "Case_NumberAlt" TEXT,
+    "Reported_Date" TIMESTAMPTZ(6) NOT NULL,
+    "Occurred_Date" TIMESTAMP(6),
+    "NIBRS_Crime_Against" TEXT NOT NULL,
+    "NIBRS_Group" TEXT NOT NULL,
+    "NIBRS_Code" TEXT NOT NULL,
+    "Offense_Category" TEXT NOT NULL,
+    "Offense" TEXT NOT NULL,
+    "Problem_Initial" TEXT,
+    "Problem_Final" TEXT,
+    "Address" TEXT,
+    "Precinct" SMALLINT,
+    "Neighborhood" TEXT,
+    "Ward" SMALLINT,
+    "Latitude" DOUBLE PRECISION NOT NULL,
+    "Longitude" DOUBLE PRECISION NOT NULL,
+    "wgsXAnon" TEXT NOT NULL,
+    "wgsYAnon" TEXT NOT NULL,
+    "Crime_Count" INTEGER NOT NULL,
+    "OBJECTID" INTEGER NOT NULL,
+
+    CONSTRAINT "MinneapolisCrimeRate_pkey" PRIMARY KEY ("OBJECTID")
+);
+
+-- CreateTable
+CREATE TABLE "MinneapolisPoliceUseOfForce" (
+    "X" TEXT NOT NULL,
+    "Y" TEXT NOT NULL,
+    "ID" INTEGER NOT NULL,
+    "CaseNumber" TEXT NOT NULL,
+    "ResponseDate" TIMESTAMPTZ(6),
+    "Problem" TEXT,
+    "Is911Call" BOOLEAN,
+    "PrimaryOffense" TEXT,
+    "SubjectInjury" BOOLEAN,
+    "ForceReportNumber" TEXT NOT NULL,
+    "SubjectRole" TEXT,
+    "SubjectRoleNumber" TEXT,
+    "ForceType" TEXT,
+    "ForceTypeAction" TEXT NOT NULL,
+    "Race" TEXT,
+    "Sex" TEXT,
+    "EventAge" TEXT,
+    "TypeOfResistance" TEXT,
+    "Precinct" TEXT,
+    "Neighborhood" TEXT,
+    "TotalCityCallsForYear" INTEGER NOT NULL,
+    "TotalPrecinctCallsForYear" TEXT,
+    "TotalNeighborhoodCallsForYear" INTEGER NOT NULL,
+    "CenterGBSID" TEXT NOT NULL,
+    "Latitude" DOUBLE PRECISION NOT NULL,
+    "Longitude" DOUBLE PRECISION NOT NULL,
+    "CenterX" TEXT NOT NULL,
+    "CenterY" TEXT NOT NULL,
+    "DateAdded" TIMESTAMPTZ(6) NOT NULL,
+    "OBJECTID" INTEGER NOT NULL,
+
+    CONSTRAINT "MinneapolisPoliceUseOfForce_pkey" PRIMARY KEY ("OBJECTID")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "question" TEXT NOT NULL,
+    "llm_response" TEXT,
+    "error" TEXT,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
 
